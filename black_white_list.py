883d6cb46991d72c5495b8f89f4c5336020c5334
Noir_Egoiste <dmitreykyrchin@gmail.com>
2024-07-13 00:54:18 +0300

    Complited

--- black_white_list.py
+++ black_white_list.py
@@ -6,89 +6,93 @@ from datetime import datetime
 PREFETCH_DIR = os.path.join(os.getenv('WINDIR'), 'Prefetch')
 OUTPUT_FILE = 'result.txt'
 
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except Exception as e:
+        return e
+
+
 def read_list(file_path):
-    with open(file_path, 'r') as file:
-        return [line.strip().upper() for line in file if line.strip()]
+    try:
+        with open(file_path, 'r', encoding="utf-8") as file:
+            return [line.strip().upper() for line in file if line.strip()]
+    except IOError as e:
+        return e
 
 
 def get_prefetch_files():
     try:
         return [f for f in os.listdir(PREFETCH_DIR) if f.endswith('.pf')]
     except WindowsError as e:
-        return print("{0}".format(e))
-
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except Exception as e:
-        return print("Ошибка ".formant(e))
-        
+        return e
 
 
 def parse_date(file_name):
     PATTERN = "%Y-%m-%d %H:%M:%S"
     full_path = os.path.join(PREFETCH_DIR, file_name)
-    last_executed = os.path.getmtime(full_path)
-    last_executed = datetime.strftime(datetime.fromtimestamp(last_executed), PATTERN)
-    return last_executed
+    try:
+        last_executed = os.path.getmtime(full_path)
+        last_executed = datetime.strftime(datetime.fromtimestamp(last_executed), PATTERN)
+        return last_executed
+    except WindowsError as e:
+        return e
+
 
 def check_software(software_list, prefetch_files, check_type):
     results = []
     for pf in prefetch_files:
         if ".EXE" in pf:
             file_name = pf.split("-")[0]
-            if check_type == 'white_list.txt':
+            if check_type == 'белый список':
                 if file_name in software_list:
                     results.append((file_name, parse_date(pf)))
-                else:
-                    results.append(('<b>{0}</b>'.format({file_name}), parse_date(pf)))
-            elif check_type == 'blacklist.txt':
-                if file_name not in software_list:
-                    results.append((file_name, parse_date(pf)))
-                else:
-                    results.append(('<b>{0}</b>'.format(file_name), parse_date(pf)))
-    return sorted(results, key=lambda x: x[0].lower())
+                results.append((file_name, "<b>{0}<\b>".format(parse_date(pf))))
+            elif check_type == 'черный список':
+                if file_name in software_list:
+                    results.append((file_name, "<b>{0}<\b>".format(parse_date(pf))))
+                results.append((file_name, parse_date(pf)))
+
+    return set(sorted(results, key=lambda x: x[0].lower()))
+
 
 def write_results(results, status):
-    with open(OUTPUT_FILE, 'w') as file:
+    with open(OUTPUT_FILE, 'w', encoding="utf-8") as file:
         for name, date in results:
-            file.write('{0} | {1}\n'.format(name, date))
+            file.write('{0} | {1}'.format(name, date)+"\n")
         file.write('\nСтатус: {0}'.format(status))
 
+
 def main():
-    software_list_file = "files.txt"# input("Введите путь к файлу со списком ПО например files.txt: ")
-    check_type = "white_list.txt"# input("Введите тип проверки (Белый список/Черный список): ")
-    if check_type.lower() == 'белый список':
-        check_type = "white_list.txt"
-    elif check_type.lower() == "черный список":
-        check_type = "black_list.txt"
-    # else:
-    #     print("Введите Белый список или Черный список")
+    white_list = read_list("white_list.txt")
+    black_list = read_list("black_list.txt")
 
-    software_list = read_list(software_list_file)
-    #print(software_list)
+    check_type = "Черный Список".lower()
+
     prefetch_files = get_prefetch_files()
-    #print(prefetch_files)
-    status = 'не соответствует'
+    status = "не соответствует"
+    results = check_software(white_list,
+                             prefetch_files,
+                             check_type)
 
-    if not prefetch_files:
-        status = 'не соответствует'
-        results = []
-    else:
-        results = check_software(software_list, prefetch_files, check_type)
-        if check_type == 'Белый список':
-            status = 'соответствует' if all('<b>{0}</b>'.format(name) not in result[0] for result in results) else 'не соответствует'
-        elif check_type == 'Черный список':
-            status = 'соответствует' if all('<b>{0}</b>'.format(name) not in result[0] for result in results) else 'не соответствует'
-    #print(status)
-    print(results)
+    if check_type == "белый список":
+        if all(map(lambda x: x[0] in white_list, results)):
+            status = "соответствует"
+        else:
+            status = "не соответствует"
+    elif check_type == "черный список":
+        if any(map(lambda x: x[0] in black_list, results)):
+            status = "не соответствует"
+        else:
+            status = "соответствует"
     write_results(results, status)
-    print('Результаты сохранены в {0}'.format(OUTPUT_FILE))
+    print("Результаты сохранены в {0}".format(OUTPUT_FILE))
 
 if __name__ == '__main__':
-    if is_admin():
-        main()
-    else:
-        ctypes.windll.shell32.ShellExecuteW(
-            None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+    # if is_admin():
+    #     main()
+    # else:
+    #     ctypes.windll.shell32.ShellExecuteW(
+    #         None, "runas", sys.executable, ' '.join(sys.argv), None, 1)
+    main()
